c56092d2a41d21f0469a411618ea472fd1bb8a5b
luka hasch <luka.hasch@gmail.com>
2024-07-19 17:37:32 +0200

    undo

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
 use std::io::{Read, Write};
 
-use flakes::*;
-
-struct Dummy;
-
-impl Builder for Dummy {
-    fn build(source: impl Read, input: &serde_json::Value, store: &mut Store<Single, Self>) -> std::io::Result<serde_json::Value> {
-        let mut buf = String::new();
-        store.open_file("./src/main.rs")?.read_to_string(&mut buf)?;
-        store.create_file("output")?.write(b"Hello, world!")?;
-        Ok(serde_json::Value::String(buf))
-    }
-}
-
 fn main() -> Result<(), std::io::Error> {
-    let store = store::<Dummy>();
-    let input = serde_json::json!({"a":2});
-    let (path, output) = store.create("dummy", "Hello, world!".as_bytes(), &input)?;
-    println!("{}", serde_json::to_string_pretty(&output)?);
     Ok(())
 }
